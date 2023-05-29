da4631d89f1c1019895cbee6dd6e26c64f4650ce
Đàm <73334675+hadam1011@users.noreply.github.com>
2023-05-29 18:43:00 +0700

    commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,16 @@ public class UserController {
 	@PostMapping("/user")
 	public User createUser(@RequestBody User user) {
 		return userService.addUser(user);
+	}
+	
+	@PutMapping("/user/{id}")
+	public User updateUser(@PathVariable String id, @RequestBody User user) {
+		user.setId(Integer.valueOf(id));
+		return userService.addUser(user);
+	}
+	
+	@DeleteMapping("/user/{id}")
+	public void deleteUser(@PathVariable String id) {
+		userService.deleteUser(Integer.valueOf(id));
 	}
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.BookCart;
+import com.example.demo.model.Books;
+import com.example.demo.model.Comment;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
@@ -13,11 +16,42 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 	
+	@Autowired
+	private CommentService serviceComment;
+	
+	@Autowired
+	private BookCartService serviceBookcart;
+	
+	@Autowired
+	private BookService serviceBook;
+	
 	public List<User> getAllUser() {
 		return repo.findAll();
 	}	
 	
 	public User addUser(User user) {
 		return repo.save(user);
+	}
+	
+	public void deleteUser(int id) {
+		// xóa comment
+		List<Comment> comments = serviceComment.getAllComment();
+		for (Comment comment : comments) {
+			if (comment.getUserid() == id) {
+				serviceComment.deleteComment(comment.getCommentid());
+				Books book = serviceBook.getBook(comment.getBookid());
+				book.setComment(book.getComment() - 1);
+				book.setStar(book.getStar() - comment.getStar());
+				serviceBook.saveBook(book);
+			}
+		}
+		
+		// Xóa sách trong giỏ hàng
+		List<BookCart> bookcarts = serviceBookcart.getAllBookCart(id);
+		for (BookCart bc : bookcarts) {
+			serviceBookcart.deleteBookCart(bc.getBook_cartid());
+		}
+		
+		repo.deleteById(id);
 	}
 }
